40591ab99d07c71b9aa1076de14c95f11113fd50
VivianHublo <vivian.sarazin@hublo.com>
2025-04-15 13:40:33 +0200

    feat: enhance issue comment handling by retrieving pull request details and logging workflow dispatch

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,15 +2,25 @@ import { Probot } from 'probot'
 import { parser } from './parser.js'
 
 export default (app: Probot) => {
+  app.log.info('*** Patrick ***')
   app.on('issue_comment.created', async (context) => {
     const inputs = parser(context)
     if (!inputs) return
-    await context.octokit.actions.createWorkflowDispatch({
-      owner: context.payload.sender.login,
-      repo: 'hublo/monorepo',
+    const repo = 'hublo/monorepo'
+    const owner = context.payload.sender.login
+    const pullRequest = await context.octokit.pulls.get({
+      owner,
+      repo,
+      pull_number: context.payload.issue.number,
+    })
+    app.log.info({ pullRequest })
+    const workflow = await context.octokit.actions.createWorkflowDispatch({
+      owner,
+      repo,
       workflow_id: 'e2e-playwright-tests.yml',
-      ref: 'main',
+      ref: pullRequest.data.head.ref,
       inputs,
     })
+    app.log.info({ workflow })
   })
 }
